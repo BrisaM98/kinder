505bede3fb69c534eb4fa7812b18665329360dc1
BrisaM98 <al179706@alumnos.uacj.mx>
2026-02-28 15:50:27 -0700

    Add files via upload

--- login.php
+++ login.php
@@ -6,12 +6,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $user = $_POST['usuario_usu'];
     $pass = $_POST['password_usu'];
 
-    // 1. Buscamos al usuario por su nombre
+    // se busca el usuario por su nombre
     $stmt = $conn->prepare("SELECT * FROM usuarios WHERE usuario_usu = ?");
     $stmt->execute([$user]);
     $usuario = $stmt->fetch();
 
-    // 2. Verificamos si existe y si la contraseña coincide con el hash encriptado
+    // se verifica si existe y si la contraseña coincide con el hash encriptado
     if ($usuario && password_verify($pass, $usuario['password_usu'])) {
         $_SESSION['id_usu'] = $usuario['id_usu'];
         $_SESSION['rol'] = $usuario['rol'];
